ff4b5a3da63e558bf520922a73e331860f909506
EnderPearl <1069442394@qq.com>
2019-03-26 14:41:15 +0800

    rewrite show.cpp and start.cpp

--- makefile
+++ makefile
@@ -2,12 +2,12 @@
 3b:3b.cpp
 	g++ 3b.cpp -o 3b -Wall -g
 show:show.cpp
-	g++ show.cpp -o show -lcurses -Wall -g
+	g++ show.cpp -o show -Lconsole -lconsole -lcurses -Wall -g
 start:start.cpp
-	g++ start.cpp -o start -Wall -g
+	g++ start.cpp -o start -lpthread -Wall -g
 release:
 	g++ 3b.cpp -o 3b -O3
-	g++ show.cpp -o show -O3
-	g++ start.cpp -o start -O3
+	g++ show.cpp -o show -Lconsole -lconsole -O3
+	g++ start.cpp -o start -lpthread -O3
 clean:
 	rm 3b show start
--- start.cpp
+++ start.cpp
@@ -1,12 +1,16 @@
-#include<iostream>
+#include<thread>
+#include<cstdlib>
+#include"console/os.h"
 using namespace std;
-double v;
 int main(){
-    int i,j,k,n,m,x,y;
-    int t;
-    // CopyFile("save1.txt","save.txt",0);
+	#ifdef WIN_OS
+	system("copy save1.txt save.txt");
+	#else
     system("cp save1.txt save.txt");
-    system("./show");
-    system("./3b");
-    return 0;
+	thread calc([]{
+		system("./3b");
+	});
+	system("./show");
+	calc.join();
+	#endif
 }
